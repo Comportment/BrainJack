eacbce893ef6c1eab8bb0f339ddd083e2998d7fd
Comportment <comportment@diax.me>
2017-05-26 18:14:34 +0100

    Initial commit

---
+++ src/me/diax/comportment/brainjack/Characters.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Comportment | comportment@diax.me
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.diax.comportment.brainjack;
+
+/**
+ * Created by Comportment at 17:26 on 26/05/17
+ * https://github.com/Comportment | comportment@diax.me
+ *
+ * @author Comportment
+ */
+public class Characters {
+
+    public static final char PLUS = '+';
+
+    public static final char MINUS = '-';
+
+    public static final char NEXT = '>';
+
+    public static final char LAST = '<';
+
+    public static final char LBRACK = '[';
+
+    public static final char RBRACK = ']';
+
+    public static final char OUT = '.';
+}
